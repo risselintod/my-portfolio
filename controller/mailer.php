ebe558d01d9fd41810655ebab42d6b0598819a2c
rissel_intod <rv.intod@gmail.com>
2019-02-26 10:47:33 +0800

    portfolio

---
+++ controller/mailer.php
@@ -0,0 +1,74 @@
+<?php 
+
+session_start();
+
+require "../../vendor/autoload.php";
+
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+
+
+$name = $_POST['name'];
+$email = $_POST['email']; 
+$message = $_POST['message'];
+
+//=====================
+
+
+
+	$mail = new PHPMailer(true); 
+
+
+
+	$staff_email = 'rv.intod@gmail.com';
+	$customer_email = $email;          //
+	$subject = 'Mail Notification From Rissel Intod';
+	$body = '<div style="text-transform:uppercase";>
+	<h3>"Message from Portfolio"</h3>
+	</div>'
+	."<div>Name: $name</div>"
+	."<div>Email: $email</div>"
+	."<div>Message: $message</div>";
+	try {
+	 
+	    $mail->SMTPDebug = 4;                                 
+	    $mail->isSMTP();                                     
+	    $mail->Host = 'smtp.gmail.com';                       
+	    $mail->SMTPAuth = true;                               
+	    $mail->Username = $staff_email;                       
+	    $mail->Password = '77Softeng99';                    
+	    $mail->SMTPSecure = 'tls';                            
+	    $mail->Port = 587;                                   
+
+	
+	    $mail->setFrom($staff_email, 'rv.intod@gmail.com');
+	    $mail->addAddress($customer_email);  
+
+
+	    $mail->isHTML(true); 
+	    $mail->Subject = $subject;
+	    $mail->Body = $body;
+
+	    
+	    header('location: ../views/home.php');
+	
+	    $mail->send();
+
+	} catch (Exception $e) {
+	    echo 'Message could not be sent. Mailer Error: ', $mail->ErrorInfo;
+	}
+
+
+<<<<<<< HEAD
+=======
+
+
+>>>>>>> 6d50435ad342406f1074535a8cd76b4cddc5e9c0
+
+	// Send email notification to customer
+	// ==============================================================================
+
+
+ ?>
