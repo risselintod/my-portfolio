25c016e564a48b9e2a4788b847b6344534319c6b
barallas.james <barallas.james@gmail.com>
2019-02-28 16:23:43 +0800

    p

--- index.php
+++ index.php
@@ -255,16 +255,12 @@
 						<div class="row">
 							<div class="col-12 col-md-6 col-lg-6">
 								<input type="text" name="name" id="name" class="form-control" placeholder="Enter your fullname * ">
-							</div>
-						</div>
-						<div class="row">
-							<div class="col-12 col-md-6 col-lg-6">
 								<input type="email" name="email" id="email" class="form-control" placeholder="Enter your email address * ">
 							</div>
 						</div>
 						
 					</div>
-					<div class="col-12 col-md-10 col-lg-10 offset-lg-2">
+					<div class="col-12 col-md-10 col-lg-6 offset-lg-1">
 						<textarea type="text" name="message" id="message" class="form-control mt-3" rows="10" placeholder="Your Message *"></textarea>
 					</div>
 					<div class="col-12 col-md-3 col-lg-3 offset-lg-4">
