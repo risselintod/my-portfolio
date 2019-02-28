bd3b3e6c4899742249bb6e9affdb7781d183ed66
barallas.james <barallas.james@gmail.com>
2019-02-28 16:25:21 +0800

    p

--- index.php
+++ index.php
@@ -251,7 +251,7 @@
 					<div class="col-12 col-md-12 col-lg-12 mb-4">
 						<h2 class="text-center"> Get in Touch </h2>
 					</div>
-					<div class="col-12 col-md-10 col-lg-10 offset-lg-2 p-2">
+					<div class="col-12 col-md-12 col-lg-12 offset-lg-2 p-2">
 						<div class="row">
 							<div class="col-12 col-md-6 col-lg-6">
 								<input type="text" name="name" id="name" class="form-control" placeholder="Enter your fullname * ">
