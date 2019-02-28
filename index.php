6a8f01e5c7d8991a721dece2675cf9bca50e98be
barallas.james <barallas.james@gmail.com>
2019-02-28 16:31:39 +0800

    p

--- index.php
+++ index.php
@@ -247,13 +247,13 @@
 			</div>
 			
 			<form action="/controller/mailer.php" method="POST">
-				<div id="get" class="row border p-5 mb-5">
+				<div id="get" class="row border mb-5">
 					<div class="col-12 col-md-12 col-lg-12 mb-4">
 						<h2 class="text-center"> Get in Touch </h2>
 					</div>
 					<div class="col-12 col-md-12 col-lg-12 offset-lg-3 p-2">
 						<div class="row">
-							<div class="col-12 col-md-6 col-lg-6">
+							<div class="col-12 col-md-5 col-lg-5">
 								<input type="text" name="name" id="name" class="form-control" placeholder="Enter your fullname * ">
 								<input type="email" name="email" id="email" class="form-control mt-4" placeholder="Enter your email address * ">
 							</div>
